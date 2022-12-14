8dd0f63211f1c7a094d307ed6f812312f246fee2
Aidan Farhi <aidanmcfarhi@gmail.com>
2022-12-13 22:14:37 -0500

    added simple todo logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
+import { useState } from 'react'
 import './App.css';
 
-function App() {
+export default function App() {
+
+  const [todos, setTodos] = useState([])
+  const [newTodo, setNewTodo] = useState('')
+
+  const addToto = () => {
+    const todoElement = <div key={todos.length}>
+      {newTodo}
+      <button onClick={() => removeTodo(todos.length)}>x</button>
+    </div>
+    setTodos([...todos, todoElement])
+    setNewTodo('')
+  }
+
+  const removeTodo = todoIndex => {
+    const newTodos = todos.filter(e => e !== todos[todoIndex])
+    setTodos(newTodos)
+  }
+
+  const handleTodoForm = event => {
+    event.preventDefault()
+    setNewTodo(event.target.value)
+  }
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Todos</h1>
+      <input type="text" value={newTodo} onChange={handleTodoForm}></input>
+      <button onClick={addToto}>+</button>
+      <div>{todos}</div>
     </div>
-  );
+  )
 }
-
-export default App;
